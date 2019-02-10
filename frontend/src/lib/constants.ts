c4f67d034df37e6b7be397e3ff413f6d1509b9ce
Justin Hallier <justin.hallier@ezyvet.com>
2019-02-11 12:39:31 +1300

    Removed frontend code from backend repo

--- frontend/src/lib/constants.ts
+++
@@ -1,9 +0,0 @@
-const API_ADDRESS = {
-    URL: 'http://localhost',
-    PORT: '8081'
-};
-
-/**
- * The url of the api to be used throughout the app
- */
-export const HOST = `${API_ADDRESS.URL}:${API_ADDRESS.PORT}`;
